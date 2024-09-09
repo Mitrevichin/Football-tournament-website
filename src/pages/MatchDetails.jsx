cf06ac6dec407edea146c6cc1721a5c75b5ff183
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-09-09 10:49:56 +0300

    Pitches and button added for the matches details component

--- src/pages/MatchDetails.jsx
+++ src/pages/MatchDetails.jsx
@@ -1,10 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import Navigation from '../components/Navigation';
+import styles from './MatchDetails.module.css';
 
 function MatchDetails() {
+  const navigate = useNavigate();
+
   return (
-    <main>
-      <h3>Match Details</h3>
+    <main className={styles.matchMain}>
       <Navigation />
+      <div className={styles.result}>
+        <span>Spain &nbsp; 2</span>
+        <span>:</span>
+        <span>0 &nbsp; Germany</span>
+      </div>
+      <section className={styles.pitchContainer}>
+        <div className={styles.pitch}></div>
+        <div className={styles.pitch}></div>
+      </section>
+      <button className={styles.btnBack} onClick={() => navigate(-1)}>
+        &larr; Back
+      </button>
     </main>
   );
 }
