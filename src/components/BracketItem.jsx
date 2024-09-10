caf011a6b4866ad602cfb750a0d833f03acf5879
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-09-10 17:04:20 +0300

    Finals matches order fixed

--- src/components/BracketItem.jsx
+++ src/components/BracketItem.jsx
@@ -9,7 +9,21 @@ function BracketItem({
   teamB,
   teamAScore,
   teamBScore,
+  date,
 }) {
+  function formatScore(score) {
+    const openParenIndex = score.indexOf('(');
+
+    if (openParenIndex === -1) {
+      return score;
+    }
+
+    const mainScore = score.slice(0, openParenIndex);
+    const additionalScore = score.slice(openParenIndex);
+    console.log('additionalScore', additionalScore);
+    return `${mainScore} ${additionalScore}`;
+  }
+
   return (
     <li className={styles.semiLi}>
       <Link
@@ -17,19 +31,25 @@ function BracketItem({
         style={{ textDecoration: 'none', color: 'black' }}
       >
         <div className={styles.liDiv}>
+          <span className={styles.text}>
+            {teamAScore.length > 2 || teamBScore.length > 2
+              ? 'Win on penalties'
+              : ''}
+          </span>
           <div>
             <span className={styles.flag}>
               <img src={flagAUrl} alt='Flag' />
             </span>
-            <span>{teamA}</span>
-            <span className={styles.lastChild}>{teamAScore}</span>
+            <span className={styles.teamName}>{teamA}</span>
+            <span className={styles.lastChild}>{formatScore(teamAScore)}</span>
           </div>
+          <time className={styles.date}>{date}</time>
           <div>
             <span className={styles.flag}>
               <img src={flagBUrl} alt='Flag' />
             </span>
-            <span>{teamB}</span>
-            <span>{teamBScore}</span>
+            <span className={styles.teamName}>{teamB}</span>
+            <span>{formatScore(teamBScore)}</span>
           </div>
         </div>
       </Link>
--- src/components/FinalsBracketView.jsx
+++ src/components/FinalsBracketView.jsx
@@ -1,149 +1,111 @@
 // import BracketItem from './BracketItem';
 // import styles from './FinalsBracketView.module.css';
 
-// function FinalsBracketView({ matchesWithDetails }) {
+// function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
+//   // Add a check to ensure matchesWithDetails exist
+//   // if (!matchesWithDetails || matchesWithDetails.length === 0) {
+//   //   return <div>Loading knockout matches...</div>;
+//   // }
+
+//   const knockoutMatches = matchesWithDetailsForTheFinals.filter(match => {
+//     const [month, day] = match.Date.split('/').map(Number);
+//     return (month === 6 && day >= 29) || month === 7;
+//   });
+
+//   // console.log('KcockoutMatches: ', knockoutMatches);
+
+//   // function sortKnockoutMatches(obj) {
+//   //   const matchesArray = Object.values(obj);
+//   //   matchesArray.sort((a, b) => Number(a.Order) - Number(b.Order));
+
+//   //   return matchesArray;
+//   // }
+
+//   // Split sorted matches into categories
+//   // const roundOf16Matches = sortedMatchesForRendering.slice(0, 8);
+//   // const quarterFinalsMatches = sortedMatchesForRendering.slice(8, 12);
+//   // const semiFinalsMatches = sortedMatchesForRendering.slice(12, 14);
+//   // const finalMatch = sortedMatchesForRendering.slice(14, 15);
+
 //   return (
 //     <>
-//       <h2 className={styles.finalHeading}>Road to the final</h2>
+//       <h2 className={styles.finalHeading}>Road to the Final</h2>
 //       <div className={styles.bracket}>
-//         <article aria-labelledby='round-1'>
-//           <h2 id='round-1'>Round of 16</h2>
+//         {/* Round of 16 */}
+//         <article aria-labelledby='round-16'>
+//           <h2 id='round-16'>Round of 16</h2>
 //           <ol>
-//             <BracketItem
-//               teamA='Spain'
-//               teamB='Georgia'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/spain-flag-png-large.png'
-//               flagBUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/1200px-Flag_of_Georgia.svg.png'
-//               teamAScore='4'
-//               teamBScore='1'
-//             />
-//             <BracketItem
-//               teamA='Germany'
-//               teamB='Denmark'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/germany-flag-png-large.png'
-//               flagBUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/512px-Flag_of_Denmark.svg.png'
-//               teamAScore='2'
-//               teamBScore='0'
-//             />
-//             <BracketItem
-//               teamA='Portugal'
-//               teamB='Slovenia'
-//               flagAUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1200px-Flag_of_Portugal.svg.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/slovenia-flag-png-large.png'
-//               teamAScore='0'
-//               teamBScore='0'
-//             />
-//             <BracketItem
-//               teamA='France'
-//               teamB='Belgium'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/france-flag-png-large.png'
-//               flagBUrl='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPqgeNAeAEwncFomFrvo_LleK3Crj3pl1STg&s'
-//               teamAScore='1'
-//               teamBScore='0'
-//             />
-//             <BracketItem
-//               teamA='Romania'
-//               teamB='Netherlands'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/romania-flag-png-large.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/netherlands-flag-png-large.png'
-//               teamAScore='0'
-//               teamBScore='3'
-//             />
-//             <BracketItem
-//               teamA='Austria'
-//               teamB='Turkey'
-//               flagAUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/800px-Flag_of_Austria.svg.png?20220412141234'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/turkey-flag-png-large.png'
-//               teamAScore='1'
-//               teamBScore='2'
-//             />
-//             <BracketItem
-//               teamA='England'
-//               teamB='Slovakia'
-//               flagAUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1280px-Flag_of_England.svg.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/slovakia-flag-png-large.png'
-//               teamAScore='2'
-//               teamBScore='1'
-//             />
-//             <BracketItem
-//               teamA='Switzerland'
-//               teamB='Italy'
-//               flagAUrl='https://content.app-sources.com/s/39330979433008693/uploads/store/Switzerland-flag-1-5957046.jpg'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/italy-flag-png-large.png'
-//               teamAScore='2'
-//               teamBScore='0'
-//             />
+//             {knockoutMatches.slice(0, 8).map(match => {
+//               return (
+//                 <BracketItem
+//                   key={match.ID}
+//                   matchID={match.ID}
+//                   teamA={match.TeamA?.Name}
+//                   teamB={match.TeamB?.Name}
+//                   teamAScore={match.Score.split('-')[0].slice(1)}
+//                   teamBScore={match.Score.split('-')[1]}
+//                   flagAUrl={match.TeamA?.Flag}
+//                   flagBUrl={match.TeamB?.Flag}
+//                 />
+//               );
+//             })}
 //           </ol>
 //         </article>
-//         <article aria-labelledby='round-2' className='secondArticle'>
-//           <h2 id='round-2'>Quater-Finals</h2>
+
+//         {/* Quarter-finals */}
+//         <article aria-labelledby='quarter-finals'>
+//           <h2 id='quarter-finals'>Quarter-finals</h2>
 //           <ol>
-//             <BracketItem
-//               teamA='Spain'
-//               teamB='Germany'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/spain-flag-png-large.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/germany-flag-png-large.png'
-//               teamAScore='2'
-//               teamBScore='1'
-//             />
-//             <BracketItem
-//               teamA='Portugal'
-//               teamB='France'
-//               flagAUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1200px-Flag_of_Portugal.svg.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/france-flag-png-large.png'
-//               teamAScore='0'
-//               teamBScore='0'
-//             />
-//             <BracketItem
-//               teamA='Netherlands'
-//               teamB='Turkey'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/netherlands-flag-png-large.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/turkey-flag-png-large.png'
-//               teamAScore='2'
-//               teamBScore='1'
-//             />
-//             <BracketItem
-//               teamA='England'
-//               teamB='Switzerland'
-//               flagAUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1280px-Flag_of_England.svg.png'
-//               flagBUrl='https://content.app-sources.com/s/39330979433008693/uploads/store/Switzerland-flag-1-5957046.jpg'
-//               teamAScore='1'
-//               teamBScore='1'
-//             />
+//             {knockoutMatches.slice(8, 12).map(match => (
+//               <BracketItem
+//                 key={match.ID}
+//                 matchID={match.ID}
+//                 teamA={match.TeamA?.Name}
+//                 teamB={match.TeamB?.Name}
+//                 teamAScore={match.Score.split('-')[0].slice(1)}
+//                 teamBScore={match.Score.split('-')[1]}
+//                 flagAUrl={match.TeamA?.Flag}
+//                 flagBUrl={match.TeamB?.Flag}
+//               />
+//             ))}
 //           </ol>
 //         </article>
-//         <article aria-labelledby='round-3'>
-//           <h2 id='round-3'>Semi-Finals</h2>
+
+//         {/* Semi-finals */}
+//         <article aria-labelledby='semi-finals'>
+//           <h2 id='semi-finals'>Semi-finals</h2>
 //           <ol>
-//             <BracketItem
-//               teamA='Spain'
-//               teamB='France'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/spain-flag-png-large.png'
-//               flagBUrl='https://www.countryflags.com/wp-content/uploads/france-flag-png-large.png'
-//               teamAScore='2'
-//               teamBScore='1'
-//             />
-//             <BracketItem
-//               teamA='Netherlands'
-//               teamB='England'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/netherlands-flag-png-large.png'
-//               flagBUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1280px-Flag_of_England.svg.png'
-//               teamAScore='1'
-//               teamBScore='2'
-//             />
+//             {knockoutMatches.slice(12, 14).map(match => (
+//               <BracketItem
+//                 key={match.ID}
+//                 matchID={match.ID}
+//                 teamA={match.TeamA?.Name}
+//                 teamB={match.TeamB?.Name}
+//                 teamAScore={match.Score.split('-')[0].slice(1)}
+//                 teamBScore={match.Score.split('-')[1]}
+//                 flagAUrl={match.TeamA?.Flag}
+//                 flagBUrl={match.TeamB?.Flag}
+//               />
+//             ))}
 //           </ol>
 //         </article>
-//         <article aria-labelledby='round-4'>
-//           <h2 id='round-4'>Final</h2>
+
+//         {/* Final */}
+//         <article aria-labelledby='final'>
+//           <h2 id='final'>Final</h2>
 //           <ol>
-//             <BracketItem
-//               teamA='Spain'
-//               teamB='England'
-//               flagAUrl='https://www.countryflags.com/wp-content/uploads/spain-flag-png-large.png'
-//               flagBUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1280px-Flag_of_England.svg.png'
-//               teamAScore='2'
-//               teamBScore='1'
-//             />
+//             {knockoutMatches.slice(14, 15).map(match => (
+//               <BracketItem
+//                 key={match.ID}
+//                 matchID={match.ID}
+//                 teamA={match.TeamA?.Name}
+//                 teamB={match.TeamB?.Name}
+//                 teamAScore={match.Score.split('-')[0].slice(1)}
+//                 teamBScore={match.Score.split('-')[1]}
+//                 flagAUrl={match.TeamA?.Flag}
+//                 flagBUrl={match.TeamB?.Flag}
+//               />
+//             ))}
 //           </ol>
 //         </article>
 //       </div>
@@ -157,17 +119,23 @@ import BracketItem from './BracketItem';
 import styles from './FinalsBracketView.module.css';
 
 function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
-  // Add a check to ensure matchesWithDetails exist
-  // if (!matchesWithDetails || matchesWithDetails.length === 0) {
-  //   return <div>Loading knockout matches...</div>;
-  // }
-
+  // Filter final matches
   const knockoutMatches = matchesWithDetailsForTheFinals.filter(match => {
     const [month, day] = match.Date.split('/').map(Number);
     return (month === 6 && day >= 29) || month === 7;
   });
 
-  console.log('KcockoutMatches: ', knockoutMatches);
+  console.log(knockoutMatches);
+
+  // Final rounds
+  const roundOf16Matches = knockoutMatches.slice(0, 8);
+  const quarterFinalsMatches = knockoutMatches.slice(8, 12);
+  const semiFinalsMatches = knockoutMatches.slice(12, 14);
+  const finalMatch = knockoutMatches.slice(14, 15);
+
+  function sortMatchesByOrder(match) {
+    return match.sort((a, b) => Number(a.Order) - Number(b.Order));
+  }
 
   return (
     <>
@@ -177,18 +145,17 @@ function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
         <article aria-labelledby='round-16'>
           <h2 id='round-16'>Round of 16</h2>
           <ol>
-            {knockoutMatches.slice(0, 8).map(match => (
-              const teamAScore = match.Score;
-              console.log(teamAScore)
+            {sortMatchesByOrder(roundOf16Matches).map(match => (
               <BracketItem
                 key={match.ID}
                 matchID={match.ID}
                 teamA={match.TeamA?.Name}
                 teamB={match.TeamB?.Name}
-                teamAScore={match.Score.split(':')[0]}
-                teamBScore={match.Score.split(':')[1]}
+                teamAScore={match.Score.split('-')[0].slice(1)}
+                teamBScore={match.Score.split('-')[1]}
                 flagAUrl={match.TeamA?.Flag}
                 flagBUrl={match.TeamB?.Flag}
+                date={match.Date}
               />
             ))}
           </ol>
@@ -198,16 +165,17 @@ function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
         <article aria-labelledby='quarter-finals'>
           <h2 id='quarter-finals'>Quarter-finals</h2>
           <ol>
-            {knockoutMatches.slice(8, 12).map(match => (
+            {sortMatchesByOrder(quarterFinalsMatches).map(match => (
               <BracketItem
                 key={match.ID}
                 matchID={match.ID}
                 teamA={match.TeamA?.Name}
                 teamB={match.TeamB?.Name}
-                teamAScore={match.Score.split(':')[0]}
-                teamBScore={match.Score.split(':')[1]}
+                teamAScore={match.Score.split('-')[0].slice(1)}
+                teamBScore={match.Score.split('-')[1]}
                 flagAUrl={match.TeamA?.Flag}
                 flagBUrl={match.TeamB?.Flag}
+                date={match.Date}
               />
             ))}
           </ol>
@@ -217,16 +185,17 @@ function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
         <article aria-labelledby='semi-finals'>
           <h2 id='semi-finals'>Semi-finals</h2>
           <ol>
-            {knockoutMatches.slice(12, 14).map(match => (
+            {sortMatchesByOrder(semiFinalsMatches).map(match => (
               <BracketItem
                 key={match.ID}
                 matchID={match.ID}
-                teamA={match.TeamA?.Name || 'TBD'}
-                teamB={match.TeamB?.Name || 'TBD'}
-                teamAScore={match.Score.split(':')[0]}
-                teamBScore={match.Score.split(':')[1]}
-                flagAUrl={match.TeamA?.Flag || ''}
-                flagBUrl={match.TeamB?.Flag || ''}
+                teamA={match.TeamA?.Name}
+                teamB={match.TeamB?.Name}
+                teamAScore={match.Score.split('-')[0].slice(1)}
+                teamBScore={match.Score.split('-')[1]}
+                flagAUrl={match.TeamA?.Flag}
+                flagBUrl={match.TeamB?.Flag}
+                date={match.Date}
               />
             ))}
           </ol>
@@ -236,16 +205,17 @@ function FinalsBracketView({ matchesWithDetailsForTheFinals }) {
         <article aria-labelledby='final'>
           <h2 id='final'>Final</h2>
           <ol>
-            {knockoutMatches.slice(14, 15).map(match => (
+            {finalMatch.map(match => (
               <BracketItem
                 key={match.ID}
                 matchID={match.ID}
-                teamA={match.TeamA?.Name || 'TBD'}
-                teamB={match.TeamB?.Name || 'TBD'}
-                teamAScore={match.Score.split(':')[0]}
-                teamBScore={match.Score.split(':')[1]}
-                flagAUrl={match.TeamA?.Flag || ''}
-                flagBUrl={match.TeamB?.Flag || ''}
+                teamA={match.TeamA?.Name}
+                teamB={match.TeamB?.Name}
+                teamAScore={match.Score.split('-')[0].slice(1)}
+                teamBScore={match.Score.split('-')[1]}
+                flagAUrl={match.TeamA?.Flag}
+                flagBUrl={match.TeamB?.Flag}
+                date={match.Date}
               />
             ))}
           </ol>
